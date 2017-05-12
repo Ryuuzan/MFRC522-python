739aca6920765d5e2d7c57929a9738895f4d6fff
Ryuuzan <alexmilo96@wanadoo.fr>
2017-05-12 13:47:50 +0200

    Enable use of data read by MFRC522_read

--- MFRC522.py
+++ MFRC522.py
@@ -341,6 +341,8 @@ class MFRC522:
     i = 0
     if len(backData) == 16:
       print "Sector "+str(blockAddr)+" "+str(backData)
+    # Enable data use 
+    return backData
   
   def MFRC522_Write(self, blockAddr, writeData):
     buff = []
